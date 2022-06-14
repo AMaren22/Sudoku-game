79b85563128efc540df404d7ad8606c216f56383
Augusto <augustomarengo@hotmail.com>
2022-06-14 17:25:19 -0300

    Creación de sistema de puntajes y primer version leaderboard

--- Scripts/juego.js
+++ Scripts/juego.js
@@ -19,19 +19,33 @@ const screen_win = document.querySelector("#winGame-container");
 const btn_win_new_game = document.querySelector("#new-game-win");
 const win_time = document.querySelector("#win-time");
 const win_mistakes = document.querySelector("#win-mistakes");
+const win_score = document.querySelector("#win-score")
 const game_mistake = document.querySelector("#mistake");
+const score_screen = document.querySelector("#score");
+const show_name = document.querySelector("leaderboard-name");
+const show_pos = document.querySelector("leaderboard-pos");
+const show_score = document.querySelector("leaderboard-score");
+const show_mistakes = document.querySelector("leaderboard-mistakes");
+const show_level = document.querySelector("leaderboard-level");
 
 const VARIABLES = {
   nivel: ["Facil", "Medio", "Dificil", "Muy Dificil"],
   nivel_index: [29, 38, 47, 56],
-  puntaje: [0.25, 0.5, 0.75, 1],
+  puntaje: [1.5, 2.5, 4, 6],
   numeros_posibles: [1, 2, 3, 4, 5, 6, 7, 8, 9],
 };
 
 const time = (seconds) => new Date(seconds * 1000).toISOString().slice(11, 19);
-
 const setPlayerName = (name) => localStorage.setItem("player-name", name);
 const getPlayerName = () => localStorage.getItem("player-name");
+const setTime = (time) => localStorage.setItem("time-info", time);
+const getTime = () => localStorage.getItem("time-info");
+const setMistakes = (mistakes) => localStorage.setItem("info-mistakes", mistakes);
+const getMistakes = () => localStorage.getItem("info-mistakes");
+const setScore = (score) => localStorage.setItem("info-score", score);
+const getScore = () => localStorage.getItem("info-score");
+const getInfoGame = () => JSON.parse(localStorage.getItem("Game-info"));
+const getContainer = () => JSON.parse(localStorage.getItem("info-container"));
 
 //----------------------------------------------------
 
@@ -39,12 +53,19 @@ const getPlayerName = () => localStorage.getItem("player-name");
 let seconds = 0;
 let pause = false;
 let level_index = 0;
-let nivel = VARIABLES.nivel_index[level_index];
+let nivel = 3;
 let timer = null;
 let su = undefined;
 let su_answer = undefined;
 let tile_select = 0;
 let mistakes = 0;
+let points = 0;
+let infoContainer = [];
+let info_game = {};
+let score = VARIABLES.puntaje[level_index];
+let realTime;
+let scoreUP = 0;
+
 
 //------------------------------------------------------
 
@@ -63,6 +84,8 @@ function startGame() {
     if (!pause) {
       seconds = seconds + 1;
       time_display.innerHTML = time(seconds);
+      realTime = time(seconds);
+    
     }
   }, 1000);
   //------------------------------------------------------
@@ -75,6 +98,8 @@ function restartScreen() {
   pause = false;
   seconds = 0;
   mistakes = 0;
+  points = 0;
+  scoreUP= 0;
   game_mistake.innerHTML = mistakes;
   time_display.innerHTML = time(seconds);
 
@@ -137,15 +162,25 @@ const inputNumber = () => {
 
         su_answer[row][col] = index + 1;
 
+        
+
         removeErrors();
         checkErrors(index + 1);
          if (checkErrors + 1 && tiles[tile_select].getAttribute("data-value") == 0) {
           mistakes += 1;
           game_mistake.innerHTML = mistakes;
+        }else{
+          points +=1;
         }
-        console.table(su_answer); 
-
-        wonGame() && winGame();
+         score_screen.innerHTML =  systemScore();
+        if(wonGame()){
+          saveGameInfo();
+          winGame();
+          loadGameInfo();
+          showResult();
+        }
+         
+        //wonGame() && winGame();
       }
     });
   });
@@ -276,14 +311,144 @@ const wonGame = () => sudokuCheck(su_answer);
 
 //-----------------------
 
+// Guardamos objeto con la info necesaria
+
+const saveGameInfo = () =>{
+
+  info_game = {
+    nameGame: getPlayerName(),
+    timeGame: getTime(),
+    mistakesGame: getMistakes(),
+    scoreGame: getScore(),
+    levelGame: VARIABLES.nivel[level_index]
+  }
+
+  localStorage.setItem("Game-info", JSON.stringify(info_game));
+
+  
+  infoContainer.push(info_game);
+
+  localStorage.setItem("info-container", JSON.stringify(infoContainer));
+  
+  
+}
+
+//----------------------------------------
+
+// Carga de informacion
+
+const loadGameInfo = () =>{
+
+}
+
+//----------------------
+
+// Ordenar resultados para la table 
+
+const resultOrder = () =>{
+  let get = getContainer();
+  let container = [get];
+ 
+  container.sort(function(a,b){
+    if(a.scoreGame < b.scoreGame){
+        return 1;
+    }
+    else if (a.scoreGame > b.scoreGame){
+        return -1;
+    }
+    else return 0;
+
+});
+
+console.log(container);
+  
+  return container;
+
+}
+
+//---------------------------------
+
+//mostrar los resultados
+
+const showResult = () =>{
+  let result = resultOrder();
+  console.log(result);
+
+  result.forEach((e) =>{
+    /* show_pos.innerHTML = index;
+    show_name.innerHTML = e.nameGame;
+    show_score.innerHTML = e.scoreGame;
+    show_mistakes.innerHTML = e.mistakesGame;
+    show_level.innerHTML = e.levelGame; */
+    
+    console.log(e.nameGame);
+    console.log(e.scoreGame);
+    console.log(e.mistakesGame);
+    console.log(e.levelGame);
+  })
+  
+}
+
+//---------------------------------------
+
+// Sistema de puntuacion
+
+
+
+const systemScore = () =>{
+
+  let timeNumber = Number(realTime.replace(/:/g,""));
+
+  let up = points * 50 * score;
+  
+  let down = mistakes * 100;
+
+
+  
+
+  if(timeNumber > 0 && timeNumber <= 40){
+    up = up * 4;
+    scoreUP = scoreUP + up;
+    points = 0;
+  }else if(timeNumber > 40 && timeNumber <= 130){
+    up = up * 2.5;
+    scoreUP = scoreUP + up;
+    points = 0;
+  }else if (timeNumber > 130 && timeNumber <= 500){
+    up = up * 1.5;
+    scoreUP = scoreUP + up;
+    points = 0;
+  }
+  else{
+    scoreUP = scoreUP + up;
+  }
+
+  let scoreGen = scoreUP - down ;
+
+  return scoreGen;
+  
+}
+
+//----------------------------------
+
 // Pantalla final
 
 const winGame = () => {
   game_screen.classList.remove("active");
 
   pause = true;
+  setTime(time(seconds));
+  
+  
+
+  
   win_time.innerHTML = time(seconds);
   win_mistakes.innerHTML = mistakes;
+  let scoreValue = systemScore();
+  win_score.innerHTML = scoreValue;
+  setScore(scoreValue);
+  setMistakes(mistakes);
+  
   screen_win.classList.add("active3");
 
   btn_win_new_game.addEventListener("click", () => {
@@ -324,6 +489,7 @@ new_game.addEventListener("click", () => {
   } else {
     startGame();
     initSudoku();
+   
   }
 });
 //-----------------------------------------------
@@ -333,12 +499,13 @@ new_game.addEventListener("click", () => {
 input_name.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     if (input_name.value.trim().length == 0) {
-      setInterval(() => {
+      setTimeout(() => {
         input_name.classList.add("error");
         input_name.focus();
       }, 500);
     } else {
       startGame();
+      initSudoku();
     }
   }
 });
@@ -348,13 +515,10 @@ input_name.addEventListener("keydown", (e) => {
 //Botón selector de niveles
 
 btn_level.addEventListener("click", (e) => {
-  if (level_index + 1 > VARIABLES.nivel_index.length - 1) {
-    level_index = 0;
-  } else {
-    level_index = level_index + 1;
-  }
+  level_index + 1 > VARIABLES.nivel_index.length - 1 ? level_index = 0 : level_index = level_index + 1;
 
   nivel = VARIABLES.nivel_index[level_index];
+  score = VARIABLES.puntaje[level_index]
   e.target.innerHTML = VARIABLES.nivel[level_index];
 });
 
@@ -402,5 +566,7 @@ btn_remove.addEventListener("click", () => {
 inputNumber();
 tileSelect();
 getPlayerName() ? (input_name.value = getPlayerName()) : input_name.focus();
+
+
 
 //----------------------------------------------------

5829bd7dc8f47327ac468395fd5b91873baa93bc
Augusto <augustomarengo@hotmail.com>
2022-06-15 16:37:10 -0300

    Creación de boton nivel y tabla, leaderboard v2

--- Scripts/juego.js
+++ Scripts/juego.js
@@ -19,20 +19,26 @@ const screen_win = document.querySelector("#winGame-container");
 const btn_win_new_game = document.querySelector("#new-game-win");
 const win_time = document.querySelector("#win-time");
 const win_mistakes = document.querySelector("#win-mistakes");
-const win_score = document.querySelector("#win-score")
+const win_score = document.querySelector("#win-score");
 const game_mistake = document.querySelector("#mistake");
 const score_screen = document.querySelector("#score");
-const show_name = document.querySelector("leaderboard-name");
-const show_pos = document.querySelector("leaderboard-pos");
-const show_score = document.querySelector("leaderboard-score");
-const show_mistakes = document.querySelector("leaderboard-mistakes");
-const show_level = document.querySelector("leaderboard-level");
+const table_screen = document.querySelector("#leaderboard-container");
+const show_name = document.querySelector("#leaderboard-name");
+const show_pos = document.querySelector("#leaderboard-pos");
+const show_score = document.querySelector("#leaderboard-score");
+const show_mistakes = document.querySelector("#leaderboard-mistakes");
+const show_level = document.querySelector("#leaderboard-level");
+const btn_table = document.querySelector("#btn-leaderboard");
+const btn_back = document.querySelector("#btn-back");
+const btn_hints = document.querySelector("#btn-hints");
+const hints_screen = document.querySelector("#hints-screen");
 
 const VARIABLES = {
   nivel: ["Facil", "Medio", "Dificil", "Muy Dificil"],
   nivel_index: [29, 38, 47, 56],
   puntaje: [1.5, 2.5, 4, 6],
   numeros_posibles: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  pista: ["On", "Off"],
 };
 
 const time = (seconds) => new Date(seconds * 1000).toISOString().slice(11, 19);
@@ -40,7 +46,8 @@ const setPlayerName = (name) => localStorage.setItem("player-name", name);
 const getPlayerName = () => localStorage.getItem("player-name");
 const setTime = (time) => localStorage.setItem("time-info", time);
 const getTime = () => localStorage.getItem("time-info");
-const setMistakes = (mistakes) => localStorage.setItem("info-mistakes", mistakes);
+const setMistakes = (mistakes) =>
+  localStorage.setItem("info-mistakes", mistakes);
 const getMistakes = () => localStorage.getItem("info-mistakes");
 const setScore = (score) => localStorage.setItem("info-score", score);
 const getScore = () => localStorage.getItem("info-score");
@@ -53,7 +60,7 @@ const getContainer = () => JSON.parse(localStorage.getItem("info-container"));
 let seconds = 0;
 let pause = false;
 let level_index = 0;
-let nivel = 3;
+let nivel = VARIABLES.nivel_index[level_index];
 let timer = null;
 let su = undefined;
 let su_answer = undefined;
@@ -65,7 +72,8 @@ let info_game = {};
 let score = VARIABLES.puntaje[level_index];
 let realTime;
 let scoreUP = 0;
-
+let pista_index = 0;
+let pista = VARIABLES.pista[pista_index];
 
 //------------------------------------------------------
 
@@ -85,13 +93,13 @@ function startGame() {
       seconds = seconds + 1;
       time_display.innerHTML = time(seconds);
       realTime = time(seconds);
-    
     }
   }, 1000);
   //------------------------------------------------------
 }
 
 function restartScreen() {
+  removeTileBoard();
   removeBoard();
   removeErrors();
   clearInterval(timer);
@@ -99,7 +107,7 @@ function restartScreen() {
   seconds = 0;
   mistakes = 0;
   points = 0;
-  scoreUP= 0;
+  scoreUP = 0;
   game_mistake.innerHTML = mistakes;
   time_display.innerHTML = time(seconds);
 
@@ -141,8 +149,10 @@ const tileSelect = () => {
         tiles.forEach((e) => e.classList.remove("tile-selected")); // esta linea elimina la casilla selecionada anteriormente para que solo aparezca una casilla marcada
         tile_select = index; // Esta linea lo que hace es guardar la posicion seleccionada para despues poder pasarle el numero en ese lugar.
         elements.classList.add("tile-selected");
-        removeTileBoard();
-        tileBoard(index);
+        if (pista_index == 0) {
+          removeTileBoard();
+          tileBoard(index);
+        }
       }
     });
   });
@@ -162,25 +172,22 @@ const inputNumber = () => {
 
         su_answer[row][col] = index + 1;
 
-        
-
         removeErrors();
         checkErrors(index + 1);
-         if (checkErrors + 1 && tiles[tile_select].getAttribute("data-value") == 0) {
+        if (
+          checkErrors + 1 &&
+          tiles[tile_select].getAttribute("data-value") == 0
+        ) {
           mistakes += 1;
           game_mistake.innerHTML = mistakes;
-        }else{
-          points +=1;
+        } else {
+          points += 1;
         }
-         score_screen.innerHTML =  systemScore();
-        if(wonGame()){
+        score_screen.innerHTML = systemScore();
+        if (wonGame()) {
           saveGameInfo();
           winGame();
-          loadGameInfo();
-          showResult();
         }
-         
-        //wonGame() && winGame();
       }
     });
   });
@@ -296,13 +303,12 @@ const tileBoard = (index) => {
   }
 };
 
-
 //-----------------------------------------------------------------
 
 //Eliminamos las ayudas anteriores
-const removeTileBoard = () =>{
-  tiles.forEach(element => element.classList.remove("tile-board"))
-}
+const removeTileBoard = () => {
+  tiles.forEach((element) => element.classList.remove("tile-board"));
+};
 
 //----------------------------------------------------
 // Verifiacion si ganó el juego
@@ -313,121 +319,103 @@ const wonGame = () => sudokuCheck(su_answer);
 
 // Guardamos objeto con la info necesaria
 
-const saveGameInfo = () =>{
-
+const saveGameInfo = () => {
   info_game = {
     nameGame: getPlayerName(),
     timeGame: getTime(),
     mistakesGame: getMistakes(),
     scoreGame: getScore(),
-    levelGame: VARIABLES.nivel[level_index]
-  }
+    levelGame: VARIABLES.nivel[level_index],
+  };
 
   localStorage.setItem("Game-info", JSON.stringify(info_game));
 
-  
   infoContainer.push(info_game);
 
   localStorage.setItem("info-container", JSON.stringify(infoContainer));
-  
-  
-}
+};
 
 //----------------------------------------
 
 // Carga de informacion
 
-const loadGameInfo = () =>{
-
-}
+const loadGameInfo = () => {};
 
 //----------------------
 
-// Ordenar resultados para la table 
+// Ordenar resultados para la table
 
-const resultOrder = () =>{
+const resultOrder = () => {
   let get = getContainer();
-  let container = [get];
- 
-  container.sort(function(a,b){
-    if(a.scoreGame < b.scoreGame){
-        return 1;
-    }
-    else if (a.scoreGame > b.scoreGame){
-        return -1;
-    }
-    else return 0;
+  let container = get;
 
-});
-
-console.log(container);
-  
+  container.sort(function (a, b) {
+    if (a.scoreGame < b.scoreGame) {
+      return 1;
+    } else if (a.scoreGame > b.scoreGame) {
+      return -1;
+    } else return 0;
+  });
   return container;
-
-}
+};
 
 //---------------------------------
 
 //mostrar los resultados
 
-const showResult = () =>{
+const showResult = () => {
   let result = resultOrder();
-  console.log(result);
 
-  result.forEach((e) =>{
-    /* show_pos.innerHTML = index;
-    show_name.innerHTML = e.nameGame;
-    show_score.innerHTML = e.scoreGame;
-    show_mistakes.innerHTML = e.mistakesGame;
-    show_level.innerHTML = e.levelGame; */
-    
-    console.log(e.nameGame);
-    console.log(e.scoreGame);
-    console.log(e.mistakesGame);
-    console.log(e.levelGame);
-  })
-  
-}
+  result.forEach((e, index) => {
+    let pos = document.createElement("span");
+    pos.innerHTML = index + 1;
+    document.getElementById("pos-container").appendChild(pos);
+    let score = document.createElement("span");
+    score.innerHTML = e.scoreGame;
+    document.getElementById("score-container").appendChild(score);
+    let name = document.createElement("span");
+    name.innerHTML = e.nameGame;
+    document.getElementById("name-container").appendChild(name);
+    let mistake = document.createElement("span");
+    mistake.innerHTML = e.mistakesGame;
+    document.getElementById("mistake-container").appendChild(mistake);
+    let level = document.createElement("span");
+    level.innerHTML = e.levelGame;
+    document.getElementById("level-container").appendChild(level);
+  });
+};
 
 //---------------------------------------
 
 // Sistema de puntuacion
 
-
-
-const systemScore = () =>{
-
-  let timeNumber = Number(realTime.replace(/:/g,""));
+const systemScore = () => {
+  let timeNumber = Number(realTime.replace(/:/g, ""));
 
   let up = points * 50 * score;
-  
+
   let down = mistakes * 100;
 
-
-  
-
-  if(timeNumber > 0 && timeNumber <= 40){
+  if (timeNumber > 0 && timeNumber <= 40) {
     up = up * 4;
     scoreUP = scoreUP + up;
     points = 0;
-  }else if(timeNumber > 40 && timeNumber <= 130){
+  } else if (timeNumber > 40 && timeNumber <= 130) {
     up = up * 2.5;
     scoreUP = scoreUP + up;
     points = 0;
-  }else if (timeNumber > 130 && timeNumber <= 500){
+  } else if (timeNumber > 130 && timeNumber <= 500) {
     up = up * 1.5;
     scoreUP = scoreUP + up;
     points = 0;
-  }
-  else{
+  } else {
     scoreUP = scoreUP + up;
   }
 
-  let scoreGen = scoreUP - down ;
+  let scoreGen = scoreUP - down;
 
   return scoreGen;
-  
-}
+};
 
 //----------------------------------
 
@@ -438,17 +426,14 @@ const winGame = () => {
 
   pause = true;
   setTime(time(seconds));
-  
-  
 
-  
   win_time.innerHTML = time(seconds);
   win_mistakes.innerHTML = mistakes;
   let scoreValue = systemScore();
   win_score.innerHTML = scoreValue;
   setScore(scoreValue);
   setMistakes(mistakes);
-  
+
   screen_win.classList.add("active3");
 
   btn_win_new_game.addEventListener("click", () => {
@@ -489,7 +474,6 @@ new_game.addEventListener("click", () => {
   } else {
     startGame();
     initSudoku();
-   
   }
 });
 //-----------------------------------------------
@@ -515,10 +499,12 @@ input_name.addEventListener("keydown", (e) => {
 //Botón selector de niveles
 
 btn_level.addEventListener("click", (e) => {
-  level_index + 1 > VARIABLES.nivel_index.length - 1 ? level_index = 0 : level_index = level_index + 1;
+  level_index + 1 > VARIABLES.nivel_index.length - 1
+    ? (level_index = 0)
+    : (level_index = level_index + 1);
 
   nivel = VARIABLES.nivel_index[level_index];
-  score = VARIABLES.puntaje[level_index]
+  score = VARIABLES.puntaje[level_index];
   e.target.innerHTML = VARIABLES.nivel[level_index];
 });
 
@@ -543,7 +529,19 @@ btn_resume.addEventListener("click", () => {
 //Boton Nuevo juego en menu de pausa
 
 btn_pause_new_game.addEventListener("click", () => {
-  restartScreen();
+  Swal.fire({
+    title: "Esta seguro que desea salir?",
+    text: "Perderá todo el progreso",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Sí, salir",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      restartScreen();
+    }
+  });
 });
 
 //-----------------------------------------------
@@ -563,10 +561,40 @@ btn_remove.addEventListener("click", () => {
 });
 
 //----------------------------------------------
+
+// Botón screen game - posiciones
+btn_table.addEventListener("click", () => {
+  screen_start.classList.add("active2");
+  showResult();
+  table_screen.classList.remove("active2");
+});
+
+//-----------------------------------------------
+
+// Botón volver (clasificaciones)
+
+btn_back.addEventListener("click", () => {
+  table_screen.classList.add("active2");
+  screen_start.classList.remove("active2");
+});
+//----------------------------------
+
+// Botón pistas
+
+btn_hints.addEventListener("click", () => {
+  pista_index + 1 > VARIABLES.pista.length - 1
+    ? (pista_index = 0)
+    : (pista_index = pista_index + 1);
+
+  hints_screen.innerHTML = VARIABLES.pista[pista_index];
+});
+
+//------------------------------------
+
 inputNumber();
 tileSelect();
 getPlayerName() ? (input_name.value = getPlayerName()) : input_name.focus();
-
-
+getInfoGame() && (info_game = getContainer());
+getContainer() && (infoContainer = getContainer());
 
 //----------------------------------------------------
